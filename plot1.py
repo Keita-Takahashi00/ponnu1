c1090b88296a589703fd57f3fd25c81751a53c76
Keita-Takahashi00 <keita.takahashi.4.biz@gmail.com>
2025-04-09 17:52:06 +0900

    Update plot1.py

--- plot1.py
+++ plot1.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-# Load the JSON file
-json_file = "./dm.json"
+# Load the CSV file
+csv_file = "./dm.csv"
 
 # Create a DataFrame
-df = pd.read_json(json_file)
+df = pd.read_csv(csv_file)
 
 # Display the DataFrame
 st.markdown("## - _DM domain_")
