ad512ab9546013bc6dbe84251f8a520bf367e75e
Keita-Takahashi00 <keita.takahashi.4.biz@gmail.com>
2025-04-10 17:16:15 +0900

    Update plot1.py

--- plot1.py
+++ plot1.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-# Load the CSV file
-csv_file = "dm.csv"
+# List of CSV files
+csv_files = ["dm.csv", "ds.csv", "se.csv", "sv.csv"]
 
-# Create a DataFrame
-df = pd.read_csv(csv_file)
+# Iterate through the list of files
+for csv_file in csv_files:
+    # Create a DataFrame
+    df = pd.read_csv(csv_file)
 
-# Display the DataFrame
-st.markdown("## - _DM domain_")
-st.write(df)
+    # Display the DataFrame with a title
+    st.markdown(f"## - _{csv_file.split('.')[0].upper()} domain_")
+    st.write(df)
