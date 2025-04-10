6a5363e5732dff6def83b631854e81b8b07aefa3
Keita-Takahashi00 <keita.takahashi.4.biz@gmail.com>
2025-04-10 17:29:19 +0900

    Update plot1.py

--- plot1.py
+++ plot1.py
@@ -17,7 +17,7 @@ for file in files:
     st.write(dataframes[file])
 
 # Create a new DataFrame with selected columns and rename 'sestdtc' to 'date'
-df = sv_df[['usubjid', 'visit', 'sestdtc']].rename(columns={'sestdtc': 'date'})
+df = sv[['usubjid', 'visit', 'sestdtc']].rename(columns={'sestdtc': 'date'})
 
 # Convert 'date' column to numeric (YYYYMMDD format)
 df['date'] = pd.to_datetime(df['date'], errors='coerce').dt.strftime('%Y%m%d').astype(float)
